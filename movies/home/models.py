8ff9626b8826c02ba7c523fcaa1876811030990d
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-23 13:14:33 +0530

    partial complete

--- movies/home/models.py
+++ movies/home/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 # Create your models here.
 class moviesinfo(models.Model):
-    name=models.CharField(max_length=122)
-    image=models.TextField()
-    release_year=models.CharField(max_length=122)
+    name=models.CharField(max_length=122,default=None, blank=True, null=True)
+    image=models.TextField(default=None, blank=True, null=True)
+    release_year=models.CharField(max_length=122,default=None, blank=True, null=True)
+    imdb=models.CharField(max_length=4,default=None, blank=True, null=True)
+    rt=models.CharField(max_length=6,default=None, blank=True, null=True)
+    gerne=models.CharField(max_length=122,default=None, blank=True, null=True)
+    desc=models.TextField(default=None, blank=True, null=True)
+    cast=models.TextField(default=None, blank=True, null=True)
+    link480p=models.TextField(default='#', blank=True, null=True)
+    link720p=models.TextField(default='#', blank=True, null=True)
+    link1080p=models.TextField(default='#', blank=True, null=True)
     def __str__(self):
         return self.name
     
