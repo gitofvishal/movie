5250d651f6d3345f16d160d9d62590850e62e4ec
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-11-12 23:43:05 +0530

    s

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -131,6 +131,5 @@ CORS_ORIGIN_ALLOW = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "https://moviespaceproj.herokuapp.com",
-    "https://gitofvishal.github.io/moviesFrontend"
+    "https://moviespaceproj.herokuapp.com"
 ]
