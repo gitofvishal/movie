6a2a221d0a5b830733d9ee699a0030ddcd2a7f46
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-12-09 00:10:35 +0530

    favicon added

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -133,5 +133,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://moviespaceproj.herokuapp.com",
     "http://127.0.0.1:5500",
-    "https://gitofvishal.github.io/moviesFrontend/"
+    "https://gitofvishal.github.io"
 ]
